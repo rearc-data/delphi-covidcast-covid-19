ff3c7c9e49d98e32bb813a866f6db50bba63484a
Derek Wolpert <me@derekwolpert.com>
2020-04-22 02:49:48 -0400

    restructured jsons, added csvs, fewer individual data files

--- pre-processing/pre-processing-code/lambda_function.py
+++ pre-processing/pre-processing-code/lambda_function.py
@@ -3,7 +3,7 @@ import json
 from source_data import source_dataset
 import boto3
 import os
-from datetime import date
+from datetime import date, datetime
 
 os.environ['AWS_DATA_PATH'] = '/opt/'
 
@@ -19,15 +19,15 @@ marketplace = boto3.client(
 
 s3_bucket = os.environ['S3_BUCKET']
 data_set_arn = os.environ['DATA_SET_ARN']
-data_set_id = data_set_arn.split("/", 1)[1]
+data_set_id = data_set_arn.split('/', 1)[1]
 product_id = os.environ['PRODUCT_ID']
 data_set_name = os.environ['DATA_SET_NAME']
 new_s3_key = data_set_name + '/dataset/'
 cfn_template = data_set_name + '/automation/cloudformation.yaml'
 post_processing_code = data_set_name + '/automation/post-processing-code.zip'
 
-today = date.today()
-revision_comment = 'Revision Updates v' + today.strftime('%Y-%m-%d')
+today = date.today().strftime('%Y-%m-%d')
+revision_comment = 'Revision Updates v' + today
 
 if not s3_bucket:
 	raise Exception("'S3_BUCKET' environment variable must be defined!")
@@ -43,8 +43,8 @@ if not product_id:
 
 
 def start_change_set(describe_entity_response, revision_arn):
+	
 	# Call AWSMarketplace Catalog's start-change-set API to add revisions to the Product
-
 	change_details = {
 		'DataSetArn': data_set_arn,
 		'RevisionArns': [
@@ -69,7 +69,7 @@ def start_change_set(describe_entity_response, revision_arn):
 
 
 def lambda_handler(event, context):
-	job_list = source_dataset(s3_bucket, new_s3_key)
+	source_dataset(s3_bucket, new_s3_key)
 
 	create_revision_response = dataexchange.create_revision(DataSetId=data_set_id)
 	revision_id = create_revision_response['Id']
@@ -84,7 +84,24 @@ def lambda_handler(event, context):
 			'ImportAssetsFromS3': {
 				'DataSetId': data_set_id,
 				'RevisionId': revision_id,
-				'AssetSources': job_list
+				'AssetSources': [
+					{
+						'Bucket': s3_bucket,
+						'Key': new_s3_key + 'covidcast_meta.csv'
+					},
+					{
+						'Bucket': s3_bucket,
+						'Key': new_s3_key + 'covidcast_meta.json'
+					},
+					{
+						'Bucket': s3_bucket,
+						'Key': new_s3_key + 'covidcast.csv'
+					},
+					{
+						'Bucket': s3_bucket,
+						'Key': new_s3_key + 'covidcast.json'
+					}
+				]
 			}
 		}
 	)
@@ -102,7 +119,7 @@ def lambda_handler(event, context):
 				continue
 			get_job_response = dataexchange.get_job(JobId=job_id)
 			if get_job_response['State'] == 'COMPLETED':
-				print("Job {} completed".format(job_id))
+				print('Job {} completed'.format(job_id))
 				completed_jobs.add(job_id)
 			if get_job_response['State'] == 'ERROR':
 				job_errors = get_job_response['Errors']
--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -1,5 +1,6 @@
 import json
-import datetime
+import csv
+from datetime import date, timedelta, datetime
 import time
 import boto3
 import os
@@ -8,13 +9,13 @@ from urllib.request import urlopen
 def query_and_save_json(meta):
 
     # Saving key terms from data to variables
-    data_source = meta["data_source"]
-    signal = meta["signal"]
-    time_type = meta["time_type"]
-    geo_type = meta["geo_type"]
-    min_time = str(meta["min_time"])
-    max_time = str(meta["max_time"])
-    num_locations = meta["num_locations"]
+    data_source = meta['data_source']
+    signal = meta['signal']
+    time_type = meta['time_type']
+    geo_type = meta['geo_type']
+    min_time = str(meta['min_time'])
+    max_time = str(meta['max_time'])
+    num_locations = meta['num_locations']
 
     # Constructs `filename` from data params
     filename = data_source + '-' + signal + '-' + time_type + '-' + geo_type
@@ -23,47 +24,49 @@ def query_and_save_json(meta):
     # `days_pre_step` calculates the max num of days that can be requested per call
     days_pre_step = int(3650 / num_locations)
 
-    # Constructs datetime variables to be used to keep track of incrementing date windows
-    start = datetime.datetime(
-        int(min_time[0:4]), int(min_time[4:6]), int(min_time[6:8])).date()
+    # Constructs date variables to be used to keep track of incrementing date windows
+    start = datetime.strptime(min_time, '%Y%m%d')
+    step = start + timedelta(days=(days_pre_step - 1))
 
-    step = start + datetime.timedelta(days=(days_pre_step - 1))
-
-    end = datetime.datetime(
-        int(max_time[0:4]), int(max_time[4:6]), int(max_time[6:8])).date()
-    
-    # List to contain data while incrementing through dates
-    data_list = []
+    end = datetime.strptime(max_time, '%Y%m%d')
 
     # Loop only while the date assigned to `start` is eariler or the same as the date assigned to `end`
     while start <= end:
         
         # Response to Delphi API
         res = urlopen('https://delphi.cmu.edu/epidata/api.php?source=covidcast&data_source=' + data_source + '&signal=' + signal + '&time_type=' +
-                                time_type + '&geo_type=' + geo_type + '&time_values=' + start.strftime('%Y%m%d') + '-' + step.strftime('%Y%m%d') + '&geo_value=*')
+                      time_type + '&geo_type=' + geo_type + '&time_values=' + start.strftime('%Y%m%d') + '-' + step.strftime('%Y%m%d') + '&geo_value=*')
         
         # Convering response to json
         data = json.load(res)
 
         # In the Delphi API, the value of `1` under the `result` key means a valid set of data was returned 
         if data['result'] == 1:
-            # Concats the data from this individual API call to the `data_list` list
-            data_list = data_list + data['epidata']
+            
+            if 'covidcast.csv' not in os.listdir('/tmp'):
+                with open('/tmp/covidcast.csv', 'a', encoding='utf-8') as c:
+                    writer = csv.DictWriter(c, fieldnames={
+                                            **data['epidata'][0], 'data_source': data_source, 'signal': signal, 'time_type': time_type, 'geo_type': geo_type})
+                    writer.writeheader()
+
+            # Appends data to json and csv
+            with open('/tmp/covidcast.json', 'a', encoding='utf-8') as j, open('/tmp/covidcast.csv', 'a', encoding='utf-8') as c:
+                for datum in data['epidata']:
+                    format = {**datum, 'data_source': data_source, 'signal': signal,
+                              'time_type': time_type, 'geo_type': geo_type}
+                    j.write(json.dumps(format) + '\n')
+                    writer = csv.DictWriter(c, fieldnames=format.keys())
+                    writer.writerow(format)
         else:
             print(data['result'], 'Failed to fetch ' + filename +
                     ' from ' + start.strftime('%Y%m%d') + ' to ' + step.strftime('%Y%m%d'))
         
         # Increments the date range by the `days_pre_step` value
-        start = start + datetime.timedelta(days=days_pre_step)
-        step = step + datetime.timedelta(days=days_pre_step)
-    
-    # If the data exists save the data to a json file
-    if len(data_list) > 0:
-        print('Saving ' + filename)
-        with open('/tmp/' + filename + '.json', 'w', encoding='utf-8') as f:
-            json.dump(data_list, f, indent=4)
-    else:
-        print('No data to save for ' + filename)
+        start = start + timedelta(days=days_pre_step)
+        step = step + timedelta(days=days_pre_step)
+
+    print('Finished appending ' + filename + ' data')
+
 
 def source_dataset(s3_bucket, new_s3_key):
 
@@ -75,31 +78,27 @@ def source_dataset(s3_bucket, new_s3_key):
 
     # In the Delphi API, the value of `1` under the `result` key means a valid set of data was returned
     if data['result'] == 1:
+
         # Iterates through the meta data, fetches data and saves a json file based on params in the meta data
         for meta in data['epidata']:
             query_and_save_json(meta)
         
-        # Saves meta data to json file
+        # Saves meta data to json and csv file
         print('Saving covidcast-meta')
-        with open('/tmp/covidcast-meta.json', 'w', encoding='utf-8') as f:
-            json.dump(data['epidata'], f, indent=4)
-       	
+
+        with open('/tmp/covidcast_meta.json', 'a', encoding='utf-8') as j, open('/tmp/covidcast_meta.csv', 'a', encoding='utf-8') as c:
+            writer = csv.DictWriter(c, fieldnames=data['epidata'][0])
+            writer.writeheader()
+            for datum in data['epidata']:
+                j.write(json.dumps(datum) + '\n')
+                writer.writerow(datum)
+
         # Creates S3 connection
-        s3 = boto3.client("s3")
+        s3 = boto3.client('s3')
 
-        # List to containing new paths to files/objects needed to be included with the ADX revision
-        job_list = []
-
-        # Looping through filenames, uploading to S3 and adding entry to `job_list` for file
+        # Looping through filenames, uploading to S3
         for filename in os.listdir('/tmp'):
             print('Uploading ' + filename)
             s3.upload_file('/tmp/' + filename, s3_bucket, new_s3_key + filename)
-            job_list.append(
-                {
-                    'Bucket': s3_bucket,
-                    'Key': new_s3_key + filename
-                }
-            )
-
-        # Returns `job_list` for use in `lambda_handler`
-        return job_list
+    else:
+        print('Failed to fetch covidcast_meta data')
